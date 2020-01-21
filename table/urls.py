d1b38500b2816f91ca0b4c4105b09eed9c49c171
Gaurav Sharma <gauravsharma727545@gmail.com>
2020-01-21 12:23:32 +0530

    Improvements

--- table/urls.py
+++ table/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home,name='home'),
+    path('download_data/',views.download_data,name='download_data')
 ]
--- table/views.py
+++ table/views.py
@@ -1,3 +1,5 @@
+import csv
+from django.http import HttpResponse
 from django.shortcuts import render
 from .models import TableData
 from django.db.models import Q
@@ -31,4 +33,20 @@ def home(request):
 
     context = {'table_data':table_data_all}
 
+
     return render(request,template_name,context)
+
+
+def download_data(request):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="db_tables.csv"'
+    writer = csv.writer(response)
+    writer.writerow(['user_id','username','contact','primary_role','additional_role','school_id','school_name','block_name','district_name','dise_code'])
+
+
+    table_data_all = TableData.objects.all().order_by('user_id')
+
+    for data in table_data_all:
+        writer.writerow([data.user_id,data.username,data.contact,data.primary_role,data.additional_role,data.school_id,data.school_name,data.block_name,data.district_name,data.dise_code])
+
+    return response
